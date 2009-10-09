cb2dff40e4b63afb5eaace89745b88a8ffad8444
asok <asok@asok.org>
2009-10-09 19:24:59 +0200

    just tiding up the code

--- spec/test_application/spec/helpers/resource_representations_default_r_spec.rb
+++ spec/test_application/spec/helpers/resource_representations_default_r_spec.rb
@@ -3,12 +3,6 @@ require 'resource_representations'
 
 describe ResourceRepresentations::DefaultRepresentation do
 
-  it " should generate proper html label" do
-    user = stub_model(User, {:nick => "some string"})
-    user = ResourceRepresentations::representation_for(user, nil )
-    ActionView::Helpers::FormTagHelper::form_tag 
-    user.nick.to_s.should == ERB::Util::html_escape(user.nick)
-  end
 
 
 end

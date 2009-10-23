2356d7fd409ae2cf170283fb37552dc8e7cd4f38
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-10-23 14:07:40 +0200

    Fixed problem with user spec

--- spec/test_application/spec/models/user_spec.rb
+++ spec/test_application/spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe User do
   before(:each) do
     @valid_attributes = {
-      :login => "value for login",
-      :email => "value for email"
+      :nick => "value for nick"
     }
   end
 

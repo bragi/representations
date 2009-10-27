9702884f940e00b9f18192f2cb3f34c9c173f832
Adam Sokolnicki <asok@asok-laptop.(none)>
2009-10-27 13:52:56 +0100

    Replaced files from local repository to undo the fatal pull

--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@ require 'view_helpers'
 require 'representations'
 ActionView::Base.send :include, Representations::ViewHelpers
 ActiveSupport::Dependencies.load_paths << "#{RAILS_ROOT}/app/representations/"
+

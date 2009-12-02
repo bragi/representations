e06859618fe4be98e17aa557f3bebede36a02ee6
Adam Sokolnicki <asok@asok-laptop.(none)>
2009-12-02 15:48:35 +0100

    Work on specs for ActiveRecordR

--- spec/test_application/spec/representations/active_record_spec.rb
+++ spec/test_application/spec/representations/active_record_spec.rb
@@ -6,7 +6,6 @@ describe Representations::ActiveRecordRepresentation do
     user = stub_model(User, {:profile => profile})
     user = Representations::representation_for(user, nil, 'user')
     user.profile.name.text_field.should == %Q{<input type="text" id="user_profile_name" name="user[profile_attributes][name]" value="some name" />}
-
   end
 
   it "when a method with the same name as associated object is called with a block and this object is nil it should do nothing" do
@@ -21,16 +20,38 @@ describe Representations::ActiveRecordRepresentation do
       @user = Representations::representation_for(user, nil, "user" )
     end
 
-    it "link with passed title should create link to show page" do
+    it "with passed title should create link to show page" do
       @user.link("title").should == '<a href="/users/1">title</a>'
     end
 
-    it "link with passed title and :view => :edit should creat link to edit page" do
+    it "with passed title and :view => :edit should creat link to edit page" do
       @user.link("title", :view => :edit).should == '<a href="/users/1/edit">title</a>'
     end
 
-    it "link without passed title should return proper html a tag with wrapped object's class name and id as a title" do
+    it "without passed title should return proper html a tag with wrapped object's class name and id as a title" do
       @user.link().should == '<a href="/users/1">User 1</a>'
+    end
+  end
+
+  describe :to_s do
+    before do
+      @user = mock_model(User)
+    end
+
+    it "should rander partial when wrapped object does have 'has_one' association with other parent model" do
+      profile = mock_model(Profile)
+      @user = Representations::representation_for(@user, nil, 'user')
+      profile = Representations::representation_for(profile, nil, 'profile', @user)
+      profile.should_receive(:partial)
+      profile.to_s
+    end
+
+    it "should not render partial when the wrapped object does not have 'has_one' association with other model" do
+      task = mock_model(Task, :user => @user)
+      @user = Representations::representation_for(@user, nil, 'user')
+      task = Representations::representation_for(task, nil, 'tasks', @user)
+      task.should_not_receive(:partial)
+      task.to_s
     end
   end
 end

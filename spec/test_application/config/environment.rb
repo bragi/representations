b6ac2bbe98bb6ba547e81703ade153778773295f
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-12-29 10:06:51 +0100

    Updated gems dependencies

--- spec/test_application/config/environment.rb
+++ spec/test_application/config/environment.rb
@@ -25,6 +25,8 @@ Rails::Initializer.run do |config|
   config.gem "giraffesoft-resource_controller", :lib => 'resource_controller'
   config.gem "masover-blankslate", :source => "http://gems.github.com", :lib => 'blankslate'
   config.gem "cucumber", :version => "= 0.5.3"
+  config.gem "cucumber-rails", :version => "= 0.2.2", :lib => "cucumber"
+  config.gem "database_cleaner", :version => "= 0.2.3"
   config.gem "webrat", :source => "http://gemcutter.org"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

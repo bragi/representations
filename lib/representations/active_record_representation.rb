c8baddfd0fccca017e99fd6b51f7b54e1ef86e5d
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-12-30 08:50:39 +0100

    Cleaned up formatting AR specs use constructor instead of helper to create objects

--- lib/representations/active_record_representation.rb
+++ lib/representations/active_record_representation.rb
@@ -1,7 +1,8 @@
 module Representations
   #Representation for ActiveRecord::Base objects
   class ActiveRecordRepresentation < Representation
-    #Render partial with the given name and given namespace as a parameter
+    
+    # Render partial with the given name and given namespace as a parameter
     def partial(partial_name, namespace = nil)
       if namespace
         @template.render(:partial => "#{namespace}/#{@value.class.to_s.pluralize.downcase}/#{partial_name}")
@@ -9,11 +10,14 @@ module Representations
         @template.render(:partial => "#{@template.polymorphic_path(@value)[/\/.*\//]}#{partial_name}")
       end
     end
-    #Render partial if it has 'has_one' association with the other model, otherwise do normal to_s
+    
+    # Render partial if it has 'has_one' association with the other model, 
+    # otherwise do normal to_s
     def to_s
       @parent && @parent.instance_variable_get(:@value).class.reflections[:"#{@name}"].macro == :has_one ? partial(@name) : super
     end
-    #Form tag, namespace depends on the namespace of the controller.
+    
+    # Form tag, namespace depends on the namespace of the controller.
     def form(path = nil, &block)
       raise "You need to provide block to form representation" unless block_given?
       content = @template.capture(self, &block)
@@ -46,18 +50,19 @@ module Representations
       @template.link_to(link_title, @template.polymorphic_path(_namespaced_value, :action => action, :anchor => anchor), options)
     end
     
-    #clone Representation object and set it's @namespace variable to required value
+    # clone Representation object and set it's @namespace variable to required value
     def namespace(a)
       namespaced_representation = self.clone
       namespaced_representation.current_namespace(a)
       namespaced_representation
     end
-    #Forwards ActiveRecord invocation and wraps result in appropriate Representation
-    #Suppose that User extends ActiveRecord::Base:
-    #ar_user = User.new
-    #ar_user.nick = 'foo'
-    #user = r(ar_user) #user is now ActiveRecordRepresentation
-    #user.nick.text_field #method_missing will be called on user with method_name = 'nick' in which new method for user will be created and will be called. The newly created method will create a new DefaultRepresentation with @value set to the string 'foo'. Next the text_field will be called on the newly created DefaultRepresentation
+    
+    # Forwards ActiveRecord invocation and wraps result in appropriate Representation
+    # Suppose that User extends ActiveRecord::Base:
+    #   ar_user = User.new
+    #   ar_user.nick = 'foo'
+    #   user = r(ar_user) #user is now ActiveRecordRepresentation
+    #   user.nick.text_field #method_missing will be called on user with method_name = 'nick' in which new method for user will be created and will be called. The newly created method will create a new DefaultRepresentation with @value set to the string 'foo'. Next the text_field will be called on the newly created DefaultRepresentation
     def method_missing(method_name, *args, &block)
       method = <<-EOF
             def #{method_name}(*args, &block)
@@ -71,6 +76,7 @@ module Representations
     end
     
     private
+    
       def _namespaced_value
         @namespace + [@value]
       end
--- spec/test_application/spec/representations/active_record_spec.rb
+++ spec/test_application/spec/representations/active_record_spec.rb
@@ -4,13 +4,13 @@ describe Representations::ActiveRecordRepresentation do
   it "when a method with the same name as associated object is called with a block it should create html tags for representations passed inside a block" do
     profile = stub_model(Profile, {:name => "some name"})
     user = stub_model(User, {:profile => profile})
-    user = Representations::representation_for(user, nil, 'user')
+    user = Representations::ActiveRecordRepresentation.new(user, nil, 'user')
     user.profile.name.text_field.should == %Q{<input type="text" id="user_profile_name" name="user[profile_attributes][name]" value="some name"/>}
   end
 
   it "when a method with the same name as associated object is called with a block and this object is nil it should do nothing" do
     user = stub_model(User, {:profile => nil})
-    user = Representations::representation_for(user, nil, 'user')
+    user = Representations::ActiveRecordRepresentation.new(user, nil, 'user')
     (user.profile{|p| p.name.text_field}).should == nil
   end
 
@@ -18,7 +18,7 @@ describe Representations::ActiveRecordRepresentation do
     before do
       @user_model = stub_model(User, :id => 1)
       @template = stub("Template", :link_to => nil, :polymorphic_path => "/default_path")
-      @user = Representations::representation_for(@user_model, @template, "user" )
+      @user = Representations::ActiveRecordRepresentation.new(@user_model, @template, "user" )
     end
 
     it "uses provided link title" do
@@ -54,16 +54,16 @@ describe Representations::ActiveRecordRepresentation do
 
     it "should rander partial when wrapped object does have 'has_one' association with other parent model" do
       profile = mock_model(Profile)
-      @user = Representations::representation_for(@user, nil, 'user')
-      profile = Representations::representation_for(profile, nil, 'profile', @user)
+      @user = Representations::ActiveRecordRepresentation.new(@user, nil, 'user')
+      profile = Representations::ActiveRecordRepresentation.new(profile, nil, 'profile', @user)
       profile.should_receive(:partial)
       profile.to_s
     end
 
     it "should not render partial when the wrapped object does not have 'has_one' association with other model" do
       task = mock_model(Task, :user => @user)
-      @user = Representations::representation_for(@user, nil, 'user')
-      task = Representations::representation_for(task, nil, 'tasks', @user)
+      @user = Representations::ActiveRecordRepresentation.new(@user, nil, 'user')
+      task = Representations::ActiveRecordRepresentation.new(task, nil, 'tasks', @user)
       task.should_not_receive(:partial)
       task.to_s
     end
